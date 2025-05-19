298615c8aafd1002f75152e073d3fc088bbff77e
Sergio <sergiodfc@gmail.com>
2025-05-19 17:00:43 -0500

    fixed bug in prepare_impute_missing at script score-model

--- src/5-score-model.py
+++ src/5-score-model.py
@@ -16,7 +16,7 @@ class ScoreModel:
         df_data_imputed = df_data_score.copy()
         df_impute_parameters = pd.read_csv(f"{self._output_path_preprocess}/impute_missing_parameters.csv")
         for col in x_cols:
-            impute_value = df_impute_parameters[df_impute_parameters["variable"]==col]["impute_value"]
+            impute_value = df_impute_parameters[df_impute_parameters["variable"]==col]["impute_value"].values[0]
             df_data_imputed[col] = df_data_imputed[col].fillna(impute_value)
         return df_data_imputed
 
